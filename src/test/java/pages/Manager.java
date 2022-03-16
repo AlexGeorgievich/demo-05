731d7b5129c9d465ca1a65b51a1cea9943c22ad7
AlexGeorgievich <alexhome1@yandex.ru>
2022-03-16 18:08:04 +0300

    last coomit

--- src/test/java/pages/Manager.java
+++ src/test/java/pages/Manager.java
@@ -1,6 +1,11 @@
 package pages;
 
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import pages.components.CalendarComponent;
 
 import static com.codeborne.selenide.Condition.text;
@@ -34,73 +39,74 @@ public class Manager {
 
 
     private CalendarComponent calendarComponent = new CalendarComponent();
-
+@Step("Open site")
     public Manager openSite() {
         open("/automation-practice-form");
         return this;
     }
-
+@Step("Verify Title")
     public Manager verifyTitle() {
         headerTitle.shouldHave(text(title));
         return this;
     }
-
+@Step("Set first Name")
     public Manager setFirstName(String firstName) {
         firstNameField.setValue(firstName);
         return this;
     }
-
+@Step("Set last Name")
     public Manager setLastName(String lastName) {
         lastNameField.setValue(lastName);
         return this;
     }
-
+@Step("Set user email")
     public Manager setUserEmail(String userEmail) {
         userEmailField.setValue(userEmail);
         return this;
     }
-
+@Step("Set telephone number")
     public Manager setUserNumber(String userNumber) {
         userNumberField.setValue(userNumber);
         return this;
     }
-
+@Step("Set subject")
     public Manager setSubjectField(String subject) {
         subjectsInputField.setValue(subject).pressEnter();
         return this;
     }
-
+@Step("Set Hobby")
     public Manager setHobby(String hobbies) {
         hobbiesField.$(byText(hobbies)).click();
         return this;
     }
+    @Step("Who are you?")
     public Manager setGender(String gender) {
         genterWrapperField.$(byText(gender)).click();
         return this;
     }
-
+@Step(" You picture")
     public Manager uploadPicture(String picture) {
         uploadPictureField.uploadFromClasspath(picture);
         return this;
     }
-
+@Step("Set current Address")
     public Manager setCurrentAddress(String currentAddress) {
         currentAddressField.setValue(currentAddress).pressEnter();
         return this;
     }
-
+@Step("Happy Birthday to you")
     public Manager setDate(String day, String month, String year) {
         dateOfBirthField.click();
         calendarComponent.setDate(day, month, year);
         return this;
     }
-
+@Step("Where are you")
     public Manager setState(String state) {
         stateButton.click();
         stateCity.setValue(state).pressEnter();
         return this;
     }
-
+@Step("You City")
     public Manager setCity(String city) {
         cityButton.click();
         cityField.setValue(city).pressEnter();
@@ -110,7 +116,7 @@ public class Manager {
     public void activateForm() {
         submitButton.click();
     }
-
+@Step(" Control you Data")
     public Manager checkForm(
             String firstName,
             String lastName,
@@ -137,6 +143,10 @@ public class Manager {
         return this;
     }
 
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return  ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
 }
 
 
--- src/test/java/tests/MainTest.java
+++ src/test/java/tests/MainTest.java
@@ -1,70 +1,103 @@
 package tests;
 
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import pages.Manager;
 
 import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Configuration.baseUrl;
 import static com.codeborne.selenide.Configuration.browserSize;
-import static com.codeborne.selenide.Selenide.screenshot;
-import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.*;
 
 public class MainTest extends BaseData {
     Manager app = new Manager();
-    static BaseData test = new BaseData();
-    String screenshotBefore =  "before";
-    String screenshotFillForm =  "fillform";
-    String screenshotAfter =  "after";
+    public static BaseData test = new BaseData();
+    public String screenshotBefore = "before";
+    public String screenshotFillForm = "fillform";
+    public String screenshotAfter = "after";
 
     //    initial
     @BeforeAll
     public static void beforeAll() {
         baseUrl = "https://demoqa.com";
-        browserSize = "1920x2080";
+        browserSize = "1920x1080";
         reportsFolder = "test-result/reports";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+
+//        Configuration.browserCapabilities = capabilities;
+//        Configuration.startMaximized = true;
+//        Configuration.remote = getSelenoidUrl();
     }
 
-    // open and fill form
     @Test
-    @DisplayName(" - Fill Form -")
+    @Description(" Работа выполнена в рамках курса QA.GURU")
+    @Link(name = "Testing", url = "https://demoqa.com")
+    @DisplayName(" - Заполнение формы и контроль результатов -")
     public void openTest() {
-        app.openSite()
-                .verifyTitle();
-        Selenide.screenshot(screenshotBefore);
+        SelenideLogger.addListener("allure", new AllureSelenide());
 
-        app.setFirstName(test.firstName)
-                .setLastName(test.lastName)
-                .setUserEmail(test.userEmail)
-                .setGender(test.genterWrapper)
-                .setUserNumber(test.userNumber)
-                .setDate(calendarDay, calendarMonth, calendarYear)
-                .setSubjectField(test.subjects)
-                .setHobby(test.hobbies)
-                .uploadPicture(test.uploadPicture)
-                .setCurrentAddress(test.currentAddress)
-                .setState(test.state)
-                .setCity(test.city);
-        Selenide.screenshot(screenshotFillForm);
-                app.activateForm();
+        Allure.step(" Открытие сайта " + baseUrl, () -> {
+            app.openSite()
+                    .verifyTitle();
+            Selenide.screenshot(screenshotBefore);
+            app.takeScreenshot();
+        });
 
+        Allure.step(" Заполнение регистрационной  формы ", () -> {
+            app.setFirstName(test.firstName)
+                    .setLastName(test.lastName)
+                    .setUserEmail(test.userEmail)
+                    .setGender(test.genterWrapper)
+                    .setUserNumber(test.userNumber)
+                    .setDate(calendarDay, calendarMonth, calendarYear)
+                    .setSubjectField(test.subjects)
+                    .setHobby(test.hobbies)
+                    .uploadPicture(test.uploadPicture)
+                    .setCurrentAddress(test.currentAddress)
+                    .setState(test.state)
+                    .setCity(test.city);
+            Selenide.screenshot(screenshotFillForm);
+            app.activateForm();
+            app.takeScreenshot();
+        });
         sleep(2000);
 // control date in form
-        app.checkForm(test.firstName,
-                test.lastName,
-                test.userEmail,
-                test.genterWrapper,
-                test.userNumber,
-                test.dateCreated,
-                test.subjects,
-                test.hobbies,
-                test.uploadPicture,
-                test.currentAddress,
-                test.state,
-                test.city);
-        Selenide.screenshot(screenshotAfter);
-
+        Allure.step(" Контроль достоверности заполнения регистрационной формы ", () -> {
+            app.checkForm(test.firstName,
+                    test.lastName,
+                    test.userEmail,
+                    test.genterWrapper,
+                    test.userNumber,
+                    test.dateCreated,
+                    test.subjects,
+                    test.hobbies,
+                    test.uploadPicture,
+                    test.currentAddress,
+                    test.state,
+                    test.city);
+            Selenide.screenshot(screenshotAfter);
+            app.takeScreenshot();
+        });
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
     }
 }
